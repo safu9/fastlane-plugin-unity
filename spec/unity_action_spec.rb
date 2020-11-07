ddc14778ed9640d6b862e8385c7fa1f22c628317
safu9 <safu9.dev@gmail.com>
2020-11-07 12:39:59 +0900

    Add test without unity_path

--- spec/unity_action_spec.rb
+++ spec/unity_action_spec.rb
@@ -9,5 +9,17 @@ describe Fastlane::Actions::UnityAction do
 
       Fastlane::Actions::UnityAction.run({ unity_path: '/path/to/unity' })
     end
+
+    it 'exec a command without unity_path' do
+      allow(Fastlane::Helper::UnityHelper).to receive(:find_unity_path).and_return("/path/to/found/unity")
+
+      expect(FastlaneCore::CommandExecutor).to receive(:execute).with(
+        command: "\"/path/to/found/unity\" -logfile",
+        print_all: true,
+        print_command: true
+      )
+
+      Fastlane::Actions::UnityAction.run({})
+    end
   end
 end
